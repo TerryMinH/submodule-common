b6b0560d3473ac74050eca1fdc7b0833ddbfdc16
huangminguang <15896938825@163.com>
2022-07-26 18:51:41 +0800

    sub module

--- util.js
+++ util.js
@@ -2,7 +2,8 @@
  * @Author: TerryMin
  * @Date: 2022-07-26 17:56:30
  * @LastEditors: TerryMin
- * @LastEditTime: 2022-07-26 17:57:28
+ * @LastEditTime: 2022-07-26 18:37:24
  * @Description: file not
  */
-console.log('子项目 branch');
+const name='张三'
+console.log('子项目 branch test');
