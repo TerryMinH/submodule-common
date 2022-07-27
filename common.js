e1caa37747a9bc4399d65c3faf00af235baf4224
huangminguang <15896938825@163.com>
2022-07-27 13:43:36 +0800

    link module

---
+++ common.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: TerryMin
+ * @Date: 2022-07-26 17:56:30
+ * @LastEditors: TerryMin
+ * @LastEditTime: 2022-07-27 13:36:26
+ * @Description: file not
+ */
+const myModule=require('submodule-utils');
+
+console.log(myModule.name);
+myModule.sayHello();
+
--- util.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: TerryMin
- * @Date: 2022-07-26 17:56:30
- * @LastEditors: TerryMin
- * @LastEditTime: 2022-07-26 18:37:24
- * @Description: file not
- */
-const name='张三'
-console.log('子项目 branch test');
